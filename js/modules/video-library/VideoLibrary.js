87909d0ce724959f2388801939fa65066fca3fb6
Alexey V. Rybakin <gentle.av@gmail.com>
2026-05-08 18:31:44 +0500

    fixed bottom panel on video tab

--- js/modules/video-library/VideoLibrary.js
+++ js/modules/video-library/VideoLibrary.js
@@ -42,6 +42,9 @@ export class VideoLibrary {
     );
     this.eventsHandler.bindEvents();
     this.loadDirectory(this.state.getCurrentPath(), false);
+    if (this.universalPlayer) {
+      this.universalPlayer.hide();
+    }
   }
 
   playVideo(path) {
